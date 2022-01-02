c56be0f7e1f36d933ae77e8f9288812b3f23427b
rohithgilla <gillarohith1@gmail.com>
2022-01-02 20:17:17 +0530

    Well add quick landing page

--- app/root.tsx
+++ app/root.tsx
@@ -26,12 +26,25 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          async
+          defer
+          data-website-id="ecf058c7-3702-4f62-ab4e-e1fee50c6cc8"
+          src="https://umami-vercel-gold.vercel.app/umami.js"
+        ></script>
       </head>
-      <body className="bg-lime-50 px-16 py-4 mx-16 my-4 text-black ">
+      <body className="bg-lime-50 px-16 py-4 mx-16 my-4 max-w-none text-black prose-xl">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
+        Built by{" "}
+        <a
+          href="https://twitter.com/gillarohith"
+          className="underline  text-sky-500 decoration-sky-500 hover:text-sky-500 visited:text-sky-600"
+        >
+          Rohith Gilla
+        </a>
       </body>
     </html>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,43 @@
 export default function Index() {
   return (
     <div className="center">
-      <h1 className="text-xl underline text-center my-2 decoration-pink-500 decoration-2">
+      <h1 className="text-2xl font-bold underline text-center my-2 decoration-pink-500 decoration-2">
         Welcome to the unofficial peerlist API
       </h1>
-      <div className="p">
+      <h2 className="text-center py-4 underline decoration-cyan-300 text-2xl mb-4 font-semibold">
         This is how you can use this API to get peerlist stats of that user.
-      </div>
+      </h2>
+
+      <ul className="list-disc">
+        <li>
+          You will need the username of the profile which you need the data
+        </li>
+        <li>The endpoint will return a JSON object of the user details</li>
+        <li>
+          End point{" "}
+          <a
+            href="https://peerlist-card.rohithgilla.workers.dev/users/rohithgilla"
+            className="underline text-sky-500 decoration-sky-500 hover:text-sky-500 visited:text-sky-600"
+          >
+            https://peerlist-card.rohithgilla.workers.dev/users/rohithgilla
+          </a>
+        </li>
+        <li>
+          You can replace <b>rohithgilla</b> with your username and get the
+          stats 🥳
+        </li>
+      </ul>
+
+      <h2 className="text-center py-4 underline decoration-cyan-300 text-2xl mb-4 font-semibold">
+        Future Roadmap
+      </h2>
+      <ul className="list-disc">
+        <li>
+          Add an endpoint which will generate SVG image which you can embed in
+          your website or github readme file
+        </li>
+        <li>Maybe a mobile app with Flutter or Expo</li>
+      </ul>
     </div>
   );
 }
