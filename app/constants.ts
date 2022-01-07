f44e78262725ddcdc211df8432e10f973753c7a9
rohithgilla <gillarohith1@gmail.com>
2022-01-08 00:05:48 +0530

    Fix regex

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL: string = "https://peerlist.io/_next/data";
 
 // REGEX
-export const BUILD_REGEX: RegExp = /"buildId":"([a-zA-Z0-9_]*)"/;
+export const BUILD_REGEX: RegExp = /"buildId":"([a-zA-Z0-9_-]*)"/;
